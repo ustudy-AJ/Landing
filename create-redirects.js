6359493353070d022867c00953495d80b3581952
Jahongir <jahongirakbarov007@gmail.com>
2025-05-19 11:51:01 +0500

    for build 2

--- create-redirects.js
+++ create-redirects.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const redirectContent = '/* /index.html 200\n';
-const distPath = path.join(__dirname, 'dist', 'landing');
+const distPath = path.join(__dirname, 'dist', 'landing','browser');
 const redirectsPath = path.join(distPath, '_redirects');
 
 fs.writeFileSync(redirectsPath, redirectContent, 'utf8');
