ee96f0cfb582e8fc29f844f220937c8b0f67d1d9
Jahongir <jahongirakbarov007@gmail.com>
2025-05-19 11:48:12 +0500

    for build

---
+++ create-redirects.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const redirectContent = '/* /index.html 200\n';
+const distPath = path.join(__dirname, 'dist', 'landing');
+const redirectsPath = path.join(distPath, '_redirects');
+
+fs.writeFileSync(redirectsPath, redirectContent, 'utf8');
+console.log(`_redirects file created at ${redirectsPath}`);
